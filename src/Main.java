63f8f39e028f8d58e1d908abc5914e50d3ff08d0
vikdays <vikdayss@gmail.com>
2024-03-18 17:37:15 +0700

    я добавила новую строчку с выводом, в которой написала важное сообщение!

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.lang.String;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello!");
+        System.out.print("Hello, world!");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("idi v popu!");
     }
 }
