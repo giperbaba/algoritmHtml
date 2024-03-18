a3d20db681042f7a0126f2987167f9a560bea562
Yuriy Yangel <arlosaneq@gmail.com>
2024-03-18 17:22:36 +0700

    Changes namespace

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.lang.String;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello, world!");
-        Scanner scan = new Scanner(System.in);
+        System.out.print("Hello!");
     }
 }
