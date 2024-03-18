56af3c62e5657bff27cb0427e8eb3cec2872bfd2
giperbaba <aue64@list.ru>
2024-03-18 17:15:03 +0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.lang.String;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello, world!");
+        Scanner scan = new Scanner(System.in);
+    }
+}
